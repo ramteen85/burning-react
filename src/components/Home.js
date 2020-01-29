724c44608712cab05b4aa232ca8e25f9c63cf7cd
Gabriel Hilti <gabriel.hilti@gmail.com>
2020-01-29 16:46:35 +1100

    home component

---
+++ src/components/Home.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import {
+  Link,
+  Route,
+  HashRouter as Router
+} from 'react-router-dom';
+
+class Home extends Compoenent {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Route />
+          <Route />
+          <Router >
+          <Login />
+          <Registration />
+        </Router
+      </div>
+    )
+  }
+}
+
+export default Home;
